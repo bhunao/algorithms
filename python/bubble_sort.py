c25538949f50e0eff57c20a6e88a6ee19fdc9102
bhunao <bhunao@gmail.com>
2024-07-13 00:22:38 -0300

    PAINT on the wall?

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,18 +1,24 @@
-def bubble_sort(collection: list[int]) -> list[int]:
-    return collection
+def bubble_sort(values: list[int]) -> list[int]:
+    for i in range(len(values)):
+        for ii in range(i, len(values)):
+            if values[i] > values[ii]:
+                values[i], values[ii] = values[ii], values[i]
+    return values
 
 
 def test_bubble_sort():
-    unsorted_list = [0, 1, 2, 3, 4, 5]
+    unsorted_list = [5, 1, 0, 3, 4, 2]
     sorted_list = [0, 1, 2, 3, 4, 5]
     assert bubble_sort(unsorted_list) == sorted_list
+    assert bubble_sort(unsorted_list) == sorted(unsorted_list)
 
 
 if __name__ == "__main__":
     from timeit import timeit
-    unsorted_list = [0, 1, 2, 3, 4, 5]
+    unsorted_list = [0, 3, 2, 1, 4, 5][::-1]
     sorted_list = [0, 1, 2, 3, 4, 5]
-    assert bubble_sort(unsorted_list) == sorted_list
+    after_sort = bubble_sort(unsorted_list)
+    assert after_sort == sorted_list
 
     n_times = 10_000
     timed = timeit(
@@ -21,6 +27,5 @@ if __name__ == "__main__":
         number=n_times
     )
     print(" Bubble Sort ".center(50, "="))
-    print(f"bubble_sort runned {n_times} times, timed in {timed:.5f}")
+    print(f"Runned {n_times} times, timed in {timed:.5f}")
     print("".center(50, "="))
-    print("Finished")
